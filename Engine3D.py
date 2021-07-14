f1cfb5327b54b4ebd371df7bfbf12ece5d8f384e
Andrei Portales <andreifan123@gmail.com>
2021-07-14 12:37:34 -0600

    agregar glLine

--- Engine3D.py
+++ Engine3D.py
@@ -1,28 +1,42 @@
 # Programa principal
-
-from gl import Renderer
+from gl import Renderer, V2, color
 
 width = 960
 height = 540
 
 rd = Renderer(width, height)
 
-rd.glClearColor(0, 0.3, 0)
+rd.glClearColor(1, 1, 1)
 rd.glClear()
 
-rd.glColor(0.2, 1, 1)
 
 
-rd.glViewPort(200, 200, 100, 100)
+# rd.glViewPort(200, 200, 100, 100)
 
+
+
+rd.glColor(1, 1, 1)
+
+# for x in range(960):
+#     for y in range(60):
+#         rd.glPoint(x, y)
+
+# rd.glLine(V2(2, 5), V2(900, 420), color=color(1, 0, 0))
+# rd.glLine(V2(0, 0), V2(40, 500), color=color(0, 1, 0))
+# rd.glLine(V2(900, 500), V2(600, 100), color=color(0, 0, 1))
+# rd.glLine(V2(200, 400), V2(500, 400), color=color(0.2, 0.2, 1))
+
+rd.glViewPort(20, 20, 200, 200)
+
+rd.glColor(0, 0, 0)
 rd.glVertex(0, 0)
 rd.glVertex(-1, -1)
 rd.glVertex(1, 1)
 rd.glVertex(-1, 1)
 rd.glVertex(1, -1)
 
+rd.glLine(V2(200, 400), V2(200, 100), color=color(0.2, 0.2, 1))
 
-# for x in range(50, 200):
-#     rd.glPoint(x, x)
+
 
 rd.glFinish('output.bmp')
--- gl.py
+++ gl.py
@@ -1,17 +1,23 @@
 import struct
+from collections import namedtuple
+
+V2 = namedtuple('Point2', ['x', 'y'])
 
 
 def char(c):
     # 1 byte
     return struct.pack('=c', c.encode('ascii'))
 
+
 def word(w):
     # 2 bytes
     return struct.pack('=h', w)
 
+
 def dword(d):
     # 4 bytes
     return struct.pack('=l', d)
+
 
 def color(r, g, b):
     # Acepta valores de 0 a 1
@@ -31,15 +37,12 @@ class Renderer(object):
     def glInit(self, width: int, height: int):
         self.clear_color = BLACK
         self.curr_color = WHITE
-        self.viewPortX = 0
-        self.viewPortY = 0
-        self.viewPortWidth = width
-        self.viewPortHeight = height
         self.glCreateWindow(width, height)
 
     def glCreateWindow(self, width: int, height: int):
         self.width = width
         self.height = height
+        self.glViewPort(0, 0, width, height)
         self.glClear()
 
     def glClearColor(self, r: float, g: float, b: float):
@@ -52,30 +55,86 @@ class Renderer(object):
     def glColor(self, r: float, g: float, b: float):
         self.curr_color = color(r, g, b)
 
-    def glPoint(self, x: int, y: int):
-        self.pixels[x][y] = self.curr_color
-
     def glViewPort(self, x: int, y: int, width: int, height: int):
         tempX = x + width
         tempY = y + height
 
         if tempX > self.width or tempY > self.height:
-            raise 'ViewPort out of range'
+            raise 'ViewPort out of screen range'
 
-        self.viewPortX = x
-        self.viewPortY = y
-        self.viewPortWidth = width
-        self.viewPortHeight = height
+        self.vpX = x
+        self.vpY = y
+        self.vpWidth = width
+        self.vpHeight = height
+
+    def glPoint_NDC(self, x: int, y: int, color=None):
+
+        if x < -1 or x > 1:
+            return
+
+        if y < -1 or y > 1:
+            return
+
+        pixelX = (x + 1) * (self.vpWidth / 2) + self.vpX
+        pixelY = (y + 1) * (self.vpHeight / 2) + self.vpY
+        if (0 <= x <= self.width) and (0 <= y <= self.height):
+            self.pixels[int(y)][int(x)] = color or self.curr_color
+
+    def glPoint(self, x: int, y: int, color=None):
+
+        if x < self.vpX or x >= self.vpX + self.vpWidth or y < self.vpY or y >= self.vpY + self.vpHeight:
+            return
+
+        if (0 <= x <= self.width) and (0 <= y <= self.height):
+            self.pixels[int(y)][int(x)] = color or self.curr_color
+
+    def glLine(self, v0: V2, v1: V2, color=None):
+        x0 = v0.x
+        x1 = v1.x
+        y0 = v0.y
+        y1 = v1.y
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        steep = dy > dx
+
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        offset = 0
+        limit = 0.5
+        m = dy / dx
+        y = y0
+
+        for x in range(x0, x1 + 1):
+            if steep:
+                self.glPoint(y, x, color=color)
+            else:
+                self.glPoint(x, y, color=color)
+
+            offset += m
+            if offset >= limit:
+                y += 1 if y0 < y1 else - 1
+                limit += 1
 
     def glVertex(self, x: int, y: int):
         if (x > 1 or x < -1) or (y > 1 or y < -1):
             raise 'Invalid vertex'
 
-        mX = int(self.viewPortWidth / 2)
-        mY = int(self.viewPortHeight / 2)
-        cX = self.viewPortX + mX + (mX * x)
-        cY = self.viewPortY + mY + (mY * y)
-        self.glPoint(cX, cY)
+        mX = int(self.vpWidth / 2)
+        mY = int(self.vpHeight / 2)
+        cX = self.vpX + mX + (mX * x)
+        cY = self.vpY + mY + (mY * y)
+        self.pixels[int(cX)][int(cY)] = self.curr_color
 
     def glFinish(self, filename: str):
         file = open(filename, 'wb')
