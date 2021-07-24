11e651d26d1eb2e36761f842c943674632b09c9b
Andrei Portales <andreifan123@gmail.com>
2021-07-24 17:12:34 -0600

    SR3: Obj Models

---
+++ SR3ObjModels_main.py
@@ -0,0 +1,12 @@
+from gl import Renderer, V2, color
+
+
+width = 960
+height = 800
+
+rd = Renderer(width, height)
+
+rd.glColor(1, 1, 1)
+
+rd.glLoadModel('models/face.obj', scale=V2(30, 30), translate=V2(width/2, height/4 - 145))
+rd.glFinish('output.bmp')
